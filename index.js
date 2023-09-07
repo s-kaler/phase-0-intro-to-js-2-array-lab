c30e7a9f88a1ad06ea0b6b00633aebf6b87eb7cf
Sundeep <sundeep.kaler@gmail.com>
2023-09-07 07:06:33 -0400

    complete lab :

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(catName){
+    return cats.push(catName);
+}
+
+function destructivelyPrependCat(catName){
+    return cats.unshift(catName);
+}
+
+function destructivelyRemoveFirstCat(){
+    return cats.shift();
+}
+
+function destructivelyRemoveLastCat(){
+    return cats.pop();
+}
+
+function appendCat(catName){
+    const newCats = [...cats, catName];
+    return newCats;
+}
+
+function prependCat(catName){
+    const newCats = [catName, ...cats];
+    return newCats;
+}
+
+function removeLastCat(){
+    const newCats = cats.slice(0, cats.length-1);
+    return newCats;
+}
+
+function removeFirstCat(){
+    const newCats = cats.slice(1);
+    return newCats;
+}
+
